6cb6f691a0004989d3967e86fbdc9284c73e3870
Lucas <lkslucas100@gmail.com>
2020-11-12 15:44:40 -0300

    Exercicios 12/11

---
+++ Exercicio1.java
@@ -0,0 +1,11 @@
+// Utilize os conhecimentos adquirido nos vídeos de introdução a java para apresentar em console
+// um pouco mais sobre você, hobbies, o que você faz nas horas vagas o que você gosta
+// e o que você não gosta... 
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Eu sou uma pessoa bem criativa, adoro design de apps, pretendo me especializar em UX e UI. \nMeu Hobbie favorito é jogar, gosto de lol, pokemon go e Genshin Impact. \nNas horas vagas basicamente eu jogo, assisto videos no youtube, netflix e navego no facebook.");
+	}
+}
---
+++ Exercicio2.java
@@ -0,0 +1,14 @@
+// Utilize os conhecimentos adquirido nos vídeos de introdução a java para apresentar
+// em console quais foram as habilidades e mentalidades que você mais usou para chegar até aqui,
+// dica (Você pode guardar o texto em uma variável do tipo String,
+// você também pode usar o \n para pular linha assim como no portugol
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("As Mentalidades que mais usei na generation foram Persistência e mentalidade de crescimento. \nAs habilidades foram atenção aos detalhes e trabalho em equipe ");
+	}
+}
+
+
